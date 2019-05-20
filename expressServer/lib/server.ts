0587189ead99f2af3cc03be11cf872e5c29e10be
Hanh Tran <tranh10@seattleu.edu>
2019-05-20 02:57:55 -0700

    reserve server.ts to its original

--- expressServer/lib/server.ts
+++ expressServer/lib/server.ts
@@ -1,16 +1,11 @@
 import app from "./app";
-import * as mongoose from 'mongoose';
+import {DataAccess} from './DataAccess';
 
 const PORT = 8080;
 
+console.log("trying to connect to MongoDB");
+DataAccess.connect();
+
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
-    
-    mongoose.connect('mongodb://127.0.0.1:27017/sample', { useNewUrlParser: true });
-    mongoose.connection.on('open', function() {
-        console.info('Conenct to mongo!');
-    });
-    mongoose.connection.on('error', function (err:any) {
-        console.error(err);
-    });
 })
